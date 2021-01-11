5a35f24830619abf78e7a4bac6ea272b5ba2d15a
GustavMaki <gustav.siljamaki@gmail.com>
2021-01-11 13:11:13 +0100

    v.1: Just nu bryter mot DRY(dont repeat yourself). Har separata if satser för varje case.

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,9 @@
-ddd
+for(var i=1; i < 21 ; i++){
+    if(Number.isInteger(i/3) && Number.isInteger(i/5)) console.log("fizzbuzz");
+
+    if(Number.isInteger(i/3)) console.log("fizz");
+
+    else if(Number.isInteger(i/5)) console.log("buzz");
+
+    else console.log(i);
+}
