632db33c0332a71a115e69b7db66f9a78a126ffd
GustavMaki <gustav.siljamaki@gmail.com>
2021-01-11 13:20:12 +0100

    v.2: Uträkningarna skrevs först på 2 ställen var så gjorde variabler för dom (DRY).

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,9 +1,13 @@
-for(var i=1; i < 21 ; i++){
-    if(Number.isInteger(i/3) && Number.isInteger(i/5)) console.log("fizzbuzz");
 
-    if(Number.isInteger(i/3)) console.log("fizz");
+for(var i = 1; i < 21 ; i++){
+    
+    var fizz = i / 3;
+    var buzz = i / 5;
 
-    else if(Number.isInteger(i/5)) console.log("buzz");
+    if(Number.isInteger(fizz) && Number.isInteger(buzz)) console.log("fizzbuzz");
 
+    if(Number.isInteger(fizz)) console.log("fizz");
+        else if(Number.isInteger(buzz)) console.log("buzz");
+        
     else console.log(i);
 }
